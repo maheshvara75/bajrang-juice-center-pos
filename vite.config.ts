f44314fbf24abcbc56c51fecfca43fa7a8f4e8b6
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2026-04-27 19:17:39 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/bajrang-juice-center-pos/',
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
